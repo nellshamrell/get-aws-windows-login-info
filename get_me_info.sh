2389750ae1d562cad177cbf4329d20f01f9c84fc
Nell Shamrell <nellshamrell@gmail.com>
2018-10-11 11:42:09 -0700

    further clarifying info
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- get_me_info.sh
+++ get_me_info.sh
@@ -5,7 +5,7 @@ for instance in $instance_id_array
 do
   echo "==============="
   echo "here is the instance info"
-  aws ec2 describe-instances --instance-ids $instance --profile habitat --region us-west-2  | jq ['.Reservations[].Instances[] | .InstanceId, .InstanceType, .PublicDnsName']
+  aws ec2 describe-instances --instance-ids $instance --profile habitat --region us-west-2  | jq ['.Reservations[].Instances[] | .InstanceId, .InstanceType, .PublicDnsName, .PublicIpAddress']
   echo
   echo "here is the password"
   aws ec2 get-password-data --instance-id $instance --priv-launch-key ~/nshamrell2.pem --profile habitat --region us-west-2 | jq .PasswordData
